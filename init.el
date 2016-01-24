354ae775d43af8f65db13a23a19f8319f98583ae
Jeff Bellegarde <bellegar@gmail.com>
2016-01-23 22:24:26 -0800

    Add a hydra for multiple cursors.

--- init.el
+++ init.el
@@ -1154,7 +1154,27 @@ end tell"
 (use-package multiple-cursors
   :bind (("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-prev-like-this)
-         ("C-c <" . mc/mark-all-like-this)))
+         ("C-c <" . multiple-cursors-hydra/body))
+  :config
+  (defhydra multiple-cursors-hydra (:hint nil)
+  "
+     ^Up^            ^Down^        ^Other^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
+^ ^             ^ ^             [_q_] Quit
+"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("r" mc/mark-all-in-region-regexp :exit t)
+  ("q" nil)))
 
 
 ;; ** travis
