a20a4c94cb6831869706a603a266fc8ecc400ead
Jeff <Bellegarde@Jeffs-MBP.home>
2016-01-31 18:52:37 -0800

    Pull defengine commands out of use-package decl.
    The compiler is not happy with them inside the use-package :config.

--- init.el
+++ init.el
@@ -1167,57 +1167,57 @@ end tell"
 ;; External search engines.
 
 (use-package engine-mode
-  :bind "C-c /"
   :config
-  (defengine amazon
-    "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=%s"
-    :keybinding "a")
+  (engine-mode))
 
-  (defengine duckduckgo
-    "https://duckduckgo.com/?q=%s"
-    :keybinding "d")
+(defengine amazon
+  "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=%s"
+  :keybinding "a")
 
-  (defengine github
-    "https://github.com/search?ref=simplesearch&q=%s"
-    :keybinding "g")
+(defengine duckduckgo
+  "https://duckduckgo.com/?q=%s"
+  :keybinding "d")
 
-  (defengine google
-    "http://www.google.com/search?ie=utf-8&oe=utf-8&q=%s")
+(defengine github
+  "https://github.com/search?ref=simplesearch&q=%s"
+  :keybinding "g")
 
-  (defengine google-images
-    "http://www.google.com/images?hl=en&source=hp&biw=1440&bih=795&gbv=2&aq=f&aqi=&aql=&oq=&q=%s")
+(defengine google
+  "http://www.google.com/search?ie=utf-8&oe=utf-8&q=%s")
 
-  (defengine google-maps
-    "http://maps.google.com/maps?q=%s"
-    :docstring "Mappin' it up.")
+(defengine google-images
+  "http://www.google.com/images?hl=en&source=hp&biw=1440&bih=795&gbv=2&aq=f&aqi=&aql=&oq=&q=%s")
 
-  (defengine project-gutenberg
-    "http://www.gutenberg.org/ebooks/search/?query=%s")
+(defengine google-maps
+  "http://maps.google.com/maps?q=%s"
+  :docstring "Mappin' it up.")
 
-  (defengine rfcs
-    "http://pretty-rfc.herokuapp.com/search?q=%s")
+(defengine project-gutenberg
+  "http://www.gutenberg.org/ebooks/search/?query=%s")
 
-  (defengine stack-overflow
-    "https://stackoverflow.com/search?q=%s")
+(defengine rfcs
+  "http://pretty-rfc.herokuapp.com/search?q=%s")
 
-  (defengine twitter
-    "https://twitter.com/search?q=%s")
+(defengine stack-overflow
+  "https://stackoverflow.com/search?q=%s")
 
-  (defengine wikipedia
-    "http://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s"
-    :keybinding "w"
-    :docstring "Searchin' the wikis.")
+(defengine twitter
+  "https://twitter.com/search?q=%s")
 
-  (defengine wiktionary
-    "https://www.wikipedia.org/search-redirect.php?family=wiktionary&language=en&go=Go&search=%s")
+(defengine wikipedia
+  "http://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s"
+  :keybinding "w"
+  :docstring "Searchin' the wikis.")
 
-  (defengine wolfram-alpha
+(defengine wiktionary
+  "https://www.wikipedia.org/search-redirect.php?family=wiktionary&language=en&go=Go&search=%s")
+
+(defengine wolfram-alpha
     "http://www.wolframalpha.com/input/?i=%s")
 
-  (defengine youtube
+(defengine youtube
     "http://www.youtube.com/results?aq=f&oq=&search_query=%s"
     :keybinding "y")
-  (engine-mode))
 
 ;; ** Multiple-cursors
 (use-package multiple-cursors
