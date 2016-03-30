eb6a1ed340fe4a4e2dc851d78444c8055ddf8136
Jeff <Bellegarde@Jeffs-MacBook-Pro.local>
2016-03-29 20:43:19 -0700

    Rename buffer in current file.

--- init.el
+++ init.el
@@ -79,6 +79,26 @@
 ;; Make backups of files, even when they're in version control
 (setq vc-make-backup-files t)
 
+;; *** Rename current buffer file
+;; From: http://whattheemacsd.com/file-defuns.el-01.html
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+
 ;; ** Base Keymaps
 ;; My plan is to setup a small number of base keymaps to hook other functioanlity to. Some commands
 ;; may still be attached to top level commands, but I want everything to be accessible using the base keymaps.
