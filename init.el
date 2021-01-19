be02bbeba367a7fa6ddf45eb0d1c1306f12342e2
Jeff Bellegarde <bellegar@gmail.com>
2021-01-18 21:07:38 -0800

    Always bootstarp straight.
    Does not start correctly without the bootstarap.

--- init.el
+++ init.el
@@ -13,17 +13,18 @@
 ;;  * Use straight for packages
 (setq straight-use-package-by-default t)
 (defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;; Uncomment this to bootstrap straight.el
+ (let ((bootstrap-file
+        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+       (bootstrap-version 5))
+   (unless (file-exists-p bootstrap-file)
+     (with-current-buffer
+         (url-retrieve-synchronously
+          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+          'silent 'inhibit-cookies)
+       (goto-char (point-max))
+       (eval-print-last-sexp)))
+   (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'diminish)
 (straight-use-package 'bind-key)
