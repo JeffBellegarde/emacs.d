9d725fa28656b9f4952b688cb78bb1cf1ba8a2ce
Jeff Bellegarde <bellegar@gmail.com>
2021-09-25 20:21:27 -0700

    Disable multiple cursors.

--- init.el
+++ init.el
@@ -1548,35 +1548,6 @@ end tell"
   "http://www.youtube.com/results?aq=f&oq=&search_query=%s"
   :keybinding "y")
 
-;; ** Multiple-cursors
-(use-package multiple-cursors
-  :bind (("C->" . mc/mark-next-like-this)
-         ("C-<" . mc/mark-prev-like-this)
-         ("C-c <" . multiple-cursors-hydra/body))
-  :config
-  (defhydra multiple-cursors-hydra (:hint nil)
-    "
-     ^Up^            ^Down^        ^Other^
-----------------------------------------------
-[_p_]   Next    [_n_]   Next    [_l_] Edit lines
-[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
-[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
-^ ^             ^ ^             [_q_] Quit
-"
-    ("l" mc/edit-lines :exit t)
-    ("a" mc/mark-all-like-this :exit t)
-    ("n" mc/mark-next-like-this)
-    ("N" mc/skip-to-next-like-this)
-    ("M-n" mc/unmark-next-like-this)
-    ("p" mc/mark-previous-like-this)
-    ("P" mc/skip-to-previous-like-this)
-    ("M-p" mc/unmark-previous-like-this)
-    ("r" mc/mark-all-in-region-regexp :exit t)
-    ("q" nil))
-  (add-to-list 'mc/unsupported-minor-modes 'region-command-mode)
-  (add-to-list 'mc/unsupported-minor-modes 'lispy-mode)
-  (define-key region-command-mode-keymap "m" #'mc/mark-next-like-this))
-
 
 ;; ** travis
 ;; requires access token to be set.
